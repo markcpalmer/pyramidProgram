907830d2ab294216d9de6534d9aa5f2352e8da2d
Mark Palmer <mark.palmer@itsalllive.com>
2019-05-15 12:35:25 -0400

    my first commit

---
+++ Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace pyramidForloops
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            for(int i=0; i<=10; i++)
+            {
+                for(int j=0; j<=i-1;j++)
+                {
+                    Console.Write(" ");
+                }
+                for (int k = 4; k <= i; k++) ;
+                {
+                    Console.Write("*");
+                }
+                Console.WriteLine();
+            }
+            Console.ReadKey();
+        }
+    }
+}
